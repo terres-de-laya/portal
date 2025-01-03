0c2bb6687bb3150827a3404ebfd38c462ae31075
Quentin Castel <quentin.castel@dataiku.com>
2025-01-03 21:56:12 +0100

    Improve back home button

--- terres-de-laya-portal/src/app/pages/faq/faq.component.ts
+++ terres-de-laya-portal/src/app/pages/faq/faq.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TuiButton, TuiIcon } from '@taiga-ui/core';
 import { TuiAccordion } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-faq',
-  imports: [TuiAccordion],
+  imports: [TuiAccordion, TuiButton, RouterLink, TuiIcon],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
